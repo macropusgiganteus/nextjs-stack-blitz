15cb677dd78cdb229067ca919dfd90c70bc34cfc
Phanupong.cm <phanupong.cm@gaitameasia.com>
2022-05-27 17:02:02 +0700

    update project about authentication

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -1,5 +1,9 @@
-export default (req, res) => {
+export default async (req, res) => {
   // Open Chrome DevTools to step through the debugger!
   // debugger;
-  res.status(200).json({ posts: [] });
+  // res.end(`Post: ${id} (only)`);
+  const a = await fetch('https://fakestoreapi.com/products')
+  const post = await a.json();
+  res.status(200).json(post)
+  // res.status(200).json({ posts: [] });
 };
